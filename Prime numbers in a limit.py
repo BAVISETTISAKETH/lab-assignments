a87b0d3d0d69936119ab9abd8d3980f083aae99d
BAVISETTISAKETH <53270452+BAVISETTISAKETH@users.noreply.github.com>
2020-12-15 10:32:31 +0530

    Create Prime numbers in a limit.py

---
+++ Prime numbers in a limit.py
@@ -0,0 +1,15 @@
+# this is the program for printing Prime numbers upto a limit
+print ('''saketh
+12191031307''')
+lower  = int(input("enter the lower limit: "))
+higher = int(input('enter the higher limit: '))
+# Prime number logic starts here
+
+for a in range(lower,higher):
+    if a > 1:
+        for i in range(2,a):
+            if (a % i == 0):
+                break
+        else:
+            print(a)
+            
