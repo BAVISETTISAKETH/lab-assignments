60083e0b73ac1a1f0a26f77cf836c1258ab30d35
BAVISETTISAKETH <53270452+BAVISETTISAKETH@users.noreply.github.com>
2020-12-15 10:33:15 +0530

    Create Swapping of numbers.py

---
+++ Swapping of numbers.py
@@ -0,0 +1,12 @@
+# Swapping of numbers
+print ('''saketh
+121910313017''')
+a= int(input('Enter the 1st number: '))
+b =int(input('Enter the 2nd numeber: '))
+print ( 'a = ' + str(a)+ ' and b = ' +str(b))
+
+# the logic starts here
+temp = a
+a = b
+b = temp
+print ( 'a = ' + str(a)+ ' and b = ' +str(b))
